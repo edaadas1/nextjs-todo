9e3945b0f845b557e17065655da64fd2ac813c75
eda adas <eda.adas54@gmail.com>
2024-01-14 16:21:15 +0300

    input alanına bir değer girildiğinde bunu bir listede name ve id değerlerinden oluşan bir obje şeklinde tutan bir yapı oluşturdum.Eğer input alanı boşsa bu alan boş geçilemez uyarısı verdirdim.

--- src/app/page.js
+++ src/app/page.js
@@ -1,36 +1,63 @@
 "use client"
 import React, { useState } from "react"
 import style from "./page.module.css"
+import { IoMdCloseCircleOutline } from "react-icons/io";
 
 export default function Home() {
 
-  const [input, setInput] = useState("");
+  const [input, setInput] = useState({ name: "", id: "" });
   const [todolist, setTodolist] = useState([]);
 
   const addFunc = () => {
-    setTodolist([...todolist, input])
-    setInput("");
+    if (input.name !== "") {
+      setTodolist([...todolist, input])
+      setInput({ name: "" });
+    }
+    else {
+      alert("Boş bırakamazsınız...")
+    }
   }
+
+  const deleteItemFunc = (id) => {
+
+  }
+
+  console.log("todolist", todolist)
 
   return (
     <div className={style.container}>
       <div className={style.todoCard}>
         <h1>TODO LIST</h1>
 
-        <div style={{ display: "flex", justifyContent: "center", alignItems: "center", gap: "5px", width: "100%" }}>
-          <input value={input} type="text" placeholder="Yapılacak bir şey yazınız..." style={{ outline: "none", border: "none", borderRadius: "3px", height: "30px", flex: "2", }}
-            onChange={(e) => setInput(e.target.value)} />
-          <button style={{ border: "none", borderRadius: "3px", height: "30px", flex: "1", }} onClick={addFunc}>ADD</button>
+        <div style={{ display: "flex", justifyContent: "center", alignItems: "center", gap: "15px", width: "100%", marginTop: "20px", marginBottom: "20px" }}>
+          <input value={input.name} type="text" placeholder="Yapılacak bir şey yazınız..." style={{ outline: "none", paddingLeft: "15px", border: "none", borderRadius: "3px", height: "30px", flex: "2", }}
+            onChange={(e) => setInput({ name: e.target.value, id: Math.random() * 100 })} />
+          <button style={{ border: "none", borderRadius: "3px", height: "30px", flex: "1", backgroundColor: "green" }} onClick={addFunc}>ADD</button>
         </div>
 
-        <div>
-          {todolist.map((item, index) => {
-            return (
-              <div key={index}>
-                <p style={{ textAlign: "center" }}>{item}</p>
-              </div>
-            )
-          })}
+
+
+        <div style={{ color: "white" }}>
+
+          {
+            todolist.length == 0 ?
+              <p>Henüz listeniz boş</p>
+
+              :
+              todolist.map((item, index) => {
+                return (
+                  <div key={index}>
+                    <div style={{ display: "flex", justifyContent: "space-between", gap: "20px" }}>
+                      <p style={{ textAlign: "center" }}>{item.name}</p>
+                      <IoMdCloseCircleOutline onClick={() => deleteItemFunc(item.id)} />
+                    </div>
+                  </div>
+                )
+              })
+
+
+          }
+
 
         </div>
       </div>
