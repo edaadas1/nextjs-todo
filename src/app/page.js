6c754fe7ff7b03bdc8ebdef260f204597ae72adb
eda adas <eda.adas54@gmail.com>
2024-01-14 15:45:26 +0300

    page.js sayfasında usesate kullandık bunu yapabilmek için de sayfaya use client özelliği kazandırdık

--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,37 @@
-import React from "react"
+"use client"
+import React, { useState } from "react"
 import style from "./page.module.css"
 
 export default function Home() {
+
+  const [input, setInput] = useState("");
+  const [todolist, setTodolist] = useState([]);
+
+  const addFunc = () => {
+    setTodolist([...todolist, input])
+    setInput("");
+  }
+
   return (
     <div className={style.container}>
       <div className={style.todoCard}>
         <h1>TODO LIST</h1>
+
         <div style={{ display: "flex", justifyContent: "center", alignItems: "center", gap: "5px", width: "100%" }}>
-          <input type="text" style={{ border: "none", borderRadius: "3px", height: "30px", flex: "2", }} />
-          <button style={{ border: "none", borderRadius: "3px", height: "30px", flex: "1", }}>Add</button>
+          <input value={input} type="text" placeholder="Yapılacak bir şey yazınız..." style={{ outline: "none", border: "none", borderRadius: "3px", height: "30px", flex: "2", }}
+            onChange={(e) => setInput(e.target.value)} />
+          <button style={{ border: "none", borderRadius: "3px", height: "30px", flex: "1", }} onClick={addFunc}>ADD</button>
+        </div>
+
+        <div>
+          {todolist.map((item, index) => {
+            return (
+              <div key={index}>
+                <p style={{ textAlign: "center" }}>{item}</p>
+              </div>
+            )
+          })}
+
         </div>
       </div>
     </div>
