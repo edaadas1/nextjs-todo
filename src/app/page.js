5553905b5147d7eebb48e0b156474fc68f7f2fa3
eda adas <eda.adas54@gmail.com>
2024-01-14 15:36:07 +0300

    input ve buton alanı oluşturuldu

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,16 @@
 import React from "react"
+import style from "./page.module.css"
 
 export default function Home() {
   return (
-    <div>
-      Eda
+    <div className={style.container}>
+      <div className={style.todoCard}>
+        <h1>TODO LIST</h1>
+        <div style={{ display: "flex", justifyContent: "center", alignItems: "center", gap: "5px", width: "100%" }}>
+          <input type="text" style={{ border: "none", borderRadius: "3px", height: "30px", flex: "2", }} />
+          <button style={{ border: "none", borderRadius: "3px", height: "30px", flex: "1", }}>Add</button>
+        </div>
+      </div>
     </div>
   )
 }
